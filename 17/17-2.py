db2a304c3b2599284b9e84c63cdf8432ac878189
matt51814 <matthawthorn@hotmail.com>
2024-12-17 22:04:49 +0000

    feat(17): not completed 17

---
+++ 17/17-2.py
@@ -0,0 +1,101 @@
+with open('17-ex.txt', 'r') as file:
+    txt = file.read().splitlines()
+
+print(txt)
+
+for i in txt:
+    if i == '':
+        continue
+    if 'A' in i:
+        A = int(i.split(' ')[-1])
+    elif 'B' in i:
+        B = int(i.split(' ')[-1])
+    elif 'C' in i:
+        C = int(i.split(' ')[-1])
+    else:
+        prog = [int(x) for x in i.split(' ')[-1].split(',')]
+
+pointer = 0
+result = []
+print(A,B,C,prog)
+A = 0
+B0 = B
+C0 = C
+print(prog)
+
+while True:
+    A += 1
+    B = B0
+    C = C0
+    result = []
+    pointer = 0
+    
+    while pointer < len(prog):
+        combo = {
+            0:0,
+            1:1,
+            2:2,
+            3:3,
+            4:A,
+            5:B,
+            6:C,
+            7:None
+        }
+
+        try:
+            opcode = prog[pointer]
+        except IndexError:
+            break
+        operand = prog[pointer+1]
+
+        match opcode:
+            case 0:
+                num = A
+                denom = 2 ** combo[operand]
+                A = int(num/denom)
+            case 1:
+                B = B ^ operand
+            case 2:
+                B = combo[operand] % 8
+            case 3:
+                if A == 0:
+                    pass
+                else:
+                    pointer = operand
+                    continue
+            case 4:
+                B = B^C
+            case 5:
+                tmp = str(combo[operand] % 8)
+                if len(tmp) > 1:
+                    for i in tmp:
+                        result.append(int(i))
+                else:
+                    result.append(int(tmp))
+            case 6:
+                num = A
+                denom = 2 ** combo[operand]
+                B = int(num/denom)
+            case 7:
+                num = A
+                denom = 2 ** combo[operand]
+                C = int(num/denom)
+            case _:
+                pass
+        
+        pointer += 2
+    
+    print(A)
+    if A == 117440:
+        print(prog)
+        print(result)
+        breakpoint()
+
+    # if prog == result:
+    #     break
+    print(A)
+
+
+print(result)
+print(A)
+    
